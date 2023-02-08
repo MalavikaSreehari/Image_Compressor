97a8a7bf91436562f91b0cfad4d96b109310e03f
MalavikaSreehari <malavikasreehari4@gmail.com>
2023-02-08 21:46:59 +0530

    second commit

--- img_compressor.py
+++ img_compressor.py
@@ -8,7 +8,4 @@ height,width = Image.size
 Img=Image.resize((height,width), PIL.Image.LANCZOS)
 save_path = asksaveasfilename()
 Img=Img.convert("RGB")
-# im = Image.open(Img)
-# rgb_im = im.convert('RGB')
-# rgb_im.save(save_path+"_resized.jpeg")
 Img.save(save_path+"_resized.jpeg")
